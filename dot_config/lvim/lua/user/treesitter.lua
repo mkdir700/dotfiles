a1c3a6ab05edd70f681b592441e99c10bfe7a93d
mkdir700 <mkdir700@gmail.com>
2023-03-10 23:31:04 +0800

    lvim: update treesitter

--- dot_config/lvim/lua/user/treesitter.lua
+++ dot_config/lvim/lua/user/treesitter.lua
@@ -3,11 +3,11 @@ local M = {}
 M.config = function()
 	vim.wo.foldmethod = "expr"
 	vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-	-- vim.wo.foldlevel = 3
-	-- vim.wo.foldminlines = 1
-	-- vim.wo.foldnestmax = 3
-	-- vim.wo.foldtext =
-	-- 	[[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
+	vim.wo.foldlevel = 3
+	vim.wo.foldminlines = 1
+	vim.wo.foldnestmax = 3
+	vim.wo.foldtext =
+		[[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
 
 	lvim.builtin.treesitter.matchup.enable = true
 	lvim.builtin.treesitter.textobjects.select = {
