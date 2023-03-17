4b7b7a26d8cc8c41c213d21d96059c3c3456144b
mkdir700 <mkdir700@gmail.com>
2023-03-17 14:52:56 +0800

    lvim: 在使用 live_greps 搜索时忽略部分文件

--- dot_config/lvim/lua/user/telescope.lua
+++ dot_config/lvim/lua/user/telescope.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.config = function()
- --  require("telescope.builtin").buffers({
+	--  require("telescope.builtin").buffers({
 	-- 	initial_mode = "insert",
 	-- 	attach_mappings = function(prompt_bufnr, map)
 	-- 		local actions = require("telescope.actions")
@@ -10,6 +10,47 @@ M.config = function()
 	-- 		return true
 	-- 	end,
 	-- })
+	lvim.builtin.telescope.defaults.file_ignore_patterns = {
+		"vendor/*",
+		"%.lock",
+		"__pycache__/*",
+		"%.sqlite3",
+		"%.ipynb",
+		"node_modules/*",
+		"%.jpg",
+		"%.jpeg",
+		"%.png",
+		"%.svg",
+		"%.otf",
+		"%.ttf",
+		".git/",
+		"%.webp",
+		".dart_tool/",
+		".github/",
+		".gradle/",
+		".idea/",
+		".settings/",
+		".vscode/",
+		"__pycache__/",
+		"build/",
+		"env/",
+		"gradle/",
+		"node_modules/",
+		"target/",
+		"%.pdb",
+		"%.dll",
+		"%.class",
+		"%.exe",
+		"%.cache",
+		"%.ico",
+		"%.pdf",
+		"%.dylib",
+		"%.jar",
+		"%.docx",
+		"%.met",
+		"smalljre_*/*",
+		".vale/",
+	}
 end
 
 return M
