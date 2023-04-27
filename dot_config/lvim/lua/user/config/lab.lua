791dc4633dfb506e56541e0264e18e5ef479bfef
mkdir700 <mkdir700@gmail.com>
2023-04-27 10:23:46 +0800

    lvim: 开启 lab.quick_data 功能

--- dot_config/lvim/lua/user/config/lab.lua
+++ dot_config/lvim/lua/user/config/lab.lua
@@ -1,10 +1,13 @@
 local M = {}
 
 M.config = function()
+	vim.list_extend(lvim.builtin.cmp.sources, {
+		{ name = "lab.quick_data", keyword_length = 4 },
+	})
 	require("lab").setup({
-    code_runner = {
-      enabled = true,
-    },
+		code_runner = {
+			enabled = true,
+		},
 		quick_data = {
 			enabled = true,
 		},
