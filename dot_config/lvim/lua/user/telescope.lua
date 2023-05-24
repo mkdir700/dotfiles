96a46b5da6f64a4f60e6f7ec22dc0aeb4caae822
mkdir700 <mkdir700@gmail.com>
2023-05-24 22:44:10 +0800

    删除无用代码

--- dot_config/lvim/lua/user/telescope.lua
+++ dot_config/lvim/lua/user/telescope.lua
@@ -1,15 +1,6 @@
 local M = {}
 
 M.config = function()
-	--  require("telescope.builtin").buffers({
-	-- 	initial_mode = "insert",
-	-- 	attach_mappings = function(prompt_bufnr, map)
-	-- 		local actions = require("telescope.actions")
-	-- 		map("i", "<C-d>", actions.delete_buffer)
-	-- 		map("n", "<C-d>", actions.delete_buffer)
-	-- 		return true
-	-- 	end,
-	-- })
 	lvim.builtin.telescope.defaults.file_ignore_patterns = {
 		"vendor/*",
 		"%.lock",
