479926f75dbdf9cfefcf6357d0c0c5a00b0cbab7
mkdir700 <mkdir700@gmail.com>
2023-05-24 22:45:17 +0800

    更新 neoai 配置

--- dot_config/lvim/lua/user/config/neoai.lua
+++ dot_config/lvim/lua/user/config/neoai.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
 	require("neoai").setup({
 		inject = {
-			cutoff_width = nil,
+			cutoff_width = 2000,
 		},
 		shortcuts = {
 			{
@@ -21,12 +21,15 @@ M.config = function()
 			},
 			{
 				name = "explain",
+				key = "<leader>ac",
 				desc = "explain selected text/code",
 				use_context = true,
 				prompt = [[
-        解释这段代码
+        请你解释这段代码的作用。
+        注意，你现在工作在一个代码编辑器中，所以你需要根据当前上下文猜测这段文本的编程语言，并以对应语言的注释形式输出。
+        比如，如果你猜测这段代码是python，那么你应该以 “# 这是一段解释” 的形式输出。
       ]],
-				modes = { "v", "V" },
+				modes = { "v" },
 				strip_function = nil,
 			},
 			{
@@ -34,9 +37,26 @@ M.config = function()
 				desc = "translate selected text to english",
 				use_context = true,
 				prompt = [[
-        请将这段文本翻译成英文, 不要输出与翻译内容无关的内容, 仅输出翻译结果, 不要输出引号
+        请将这段文本翻译成英文, 不要输出与翻译内容无关的内容, 仅输出翻译结果, 不要输出引号。
+        注意，你现在工作在一个代码编辑器中，所以你需要根据当前上下文猜测这段文本的编程语言，并以对应语言的注释形式输出。
+        比如，如果你猜测这段文本是python，原文是 “# 这是一段文本”，那么你应该以 “# This is a text” 的形式输出。
       ]],
-				modes = { "v", "V" },
+				modes = { "v" },
+				strip_function = nil,
+			},
+			{
+				name = "gitcommit",
+				key = "<leader>aG",
+				desc = "generate git commit message",
+				use_context = false,
+				prompt = function()
+					return [[
+                    Using the following git diff generate a consise and
+                    clear git commit message, with a short title summary
+                    that is 75 characters or less:
+                ]] .. vim.fn.system("git diff")
+				end,
+				modes = { "n" },
 				strip_function = nil,
 			},
 		},
