1137e84ce28431508d1c3a5a0ae7e336bec5ffe5
mkdir700 <mkdir700@gmail.com>
2023-05-24 09:31:38 +0800

    增加别名

---
+++ dot_config/private_fish/functions/g++.fish
@@ -0,0 +1,3 @@
+function g++ --description 'alias g++=g++ -std=c++11'
+ command g++ -std=c++11 $argv; 
+end
---
+++ dot_config/private_fish/functions/py3.fish
@@ -0,0 +1,3 @@
+function py3 --wraps=python3 --description 'alias py3=python3'
+  python3 $argv; 
+end
